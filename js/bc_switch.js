eff7d06e5275762997f921ac4a43fa445373bb26
nschatz27 <akschatz1s@capetigers.com>
2026-02-26 09:12:09 -0600

    finish

--- js/bc_switch.js
+++ js/bc_switch.js
@@ -22,7 +22,7 @@ window.addEventListener("load", setupStyles);
 function setupStyles() {
    // Create a link element for the page view styles
    var pageStyle = document.createElement("link");
-   pageStyle = document.createElement("href", "bc_page.css");
+   pageStyle.setAttribute("href", "../css/bc_page.css");
    pageStyle.setAttribute("rel", "stylesheet");
    pageStyle.setAttribute("disabled", "disabled");
    
@@ -31,12 +31,12 @@ function setupStyles() {
    pageStyle.disabled = true;
 
 // Insert buttons for the style switcher
-var buttonDIV = document.createElement("div")
-buttonDIV.setAttribute("type", "button");
+var buttonDIV = document.createElement("div");
+buttonDIV.setAttribute("id", "styleButtons");
 
 var webButton = document.createElement("input");
 webButton.setAttribute("type", "button");
-webButton.setAttribute("type", "Web View");
+webButton.setAttribute("value", "Web View");
 
 var pageButton = document.createElement("input");
 pageButton.setAttribute("type", "button");
@@ -59,7 +59,7 @@ document.styleSheets[document.styleSheets.length-1].insertRule("div#styleButtons
 document.styleSheets[document.styleSheets.length-1].insertRule(
    "div#styleButtons input { \
    background-color: rgba(68, 94, 186, 0.6); \
-   border: 4px solid rgba(0, 24, 123, 0.6); \
+   border: 3px solid rgba(0, 24, 123, 0.6); \
    border-radius: 50%; \
    cursor: pointer; \
    color: white; \
@@ -69,6 +69,22 @@ document.styleSheets[document.styleSheets.length-1].insertRule(
    margin: 5px 10px; \
    width: 100px; \
 }", 1);
+
+document.styleSheets[document.styleSheets.length-1].insertRule(
+   "@media print { \
+   div#styleButtons { \
+   display: none; \
+} \
+}", 2);
+
+// Turn the Page View style off and on
+webButton.onclick = function() {
+   pageStyle.disabled = true;
+}
+
+pageButton.onclick = function() {
+   pageStyle.disabled = false;
+};
 
 }
 
